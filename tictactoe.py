eb283311204079b2e79f26f4047a97914013fe6a
AlhadiSiddig <elhadi.cp5@gmail.com>
2024-01-17 21:34:13 +0200

    tictactoe edited , with test case

--- tictactoe.py
+++ tictactoe.py
@@ -1,20 +1,39 @@
+#this solution isn't mine, i copies it from else where, though i liked it . 
+
+
 def tictactoe(moves : list[list[int]]) -> str : 
-    #split the moves list into two lists , using index : 
-    the_original_place = {1: [0 , 0 ],2 : [0,1] , 3 : [0,2] , 
-                          4: [1 , 0 ],5 : [1,1] , 6 : [1,2] , 
-                          7 : [2, 0 ] , 8 : [2,1] , 9 : [2,2]}
+    winner = None 
+
+    matrix = [[0 for _ in range(3)]  for _ in range(3)] 
     
-    player_one = [] 
-    player_two = []
+
+    for i , (x,y) in enumerate(moves): 
+        matrix[x][y] = 5 if i & 1 else 1 
+
+    def checkWin(s): 
+        nonlocal winner 
+        if s == 3 : 
+            winner = 'A' 
+        if s == 15 : 
+            winner = 'B' 
+    def checkRows(): 
+        for row in matrix : checkWin(sum(row)) 
+    def checkCols(): 
+        for col in zip(*matrix): 
+            checkWin(sum(col)) 
+    def checkDiagonal(): 
+        checkWin(matrix[0][0] + matrix[1][1] + matrix[2][2]) 
+        
+    def checkDiagonal2(): 
+        checkWin(matrix[0][2] + matrix[1][1] + matrix[2][0]) 
+
     
-    for i in moves[2:] : 
-        if moves.index(i) % 2 == 0 : 
-            player_one.append(i)
-        else : 
-            player_two.append(i) 
-
-    print(player_one , player_two)
-
-if __name__ == '__main__': 
-    assert tictactoe([[0,0] , [2,0] , [1,1] , [2,1]  , [2,2]])
-
+    checkRows() 
+    checkCols() 
+    checkDiagonal() 
+    checkDiagonal2() 
+    return winner or ('Draw' if len(moves) == 9 else 'pending ')
+    
+if __name__ =='__main__': 
+    assert tictactoe([[0,0],[2,0] , [1,1], [2,1] , [2,2]]) == 'A' , 'player B is not the winner, A is  ' 
+    print('all tests passed ') 
