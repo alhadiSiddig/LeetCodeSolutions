e742214594d424fcc7658b01ea89d625c75dc26f
AlhadiSiddig <elhadi.cp5@gmail.com>
2024-01-21 11:44:17 +0200

    largestPerimeter

--- LargestPerimeter.py
+++ LargestPerimeter.py
@@ -1,3 +1,9 @@
 '''two conditions must be satisfied :
 1. the sum of two larger than the third
 2. teh difference between two is less than the third   '''
+def largestPerimeter(nums : list[int]) -> bool : 
+    nums.sort(reverse = True ) 
+    for i in range(len(nums) -2 ): 
+        if sum(nums [i : i + 3 ]) >  2 * nums[i]: 
+            return sum(nums[i : i + 3]) 
+    return 0 
